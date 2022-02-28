b231463878d319a9bd9646970295dfd9a0dbe53e
Hetrajsinh Raj <96659551+Hetrajs@users.noreply.github.com>
2022-02-28 19:18:55 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,13 +134,13 @@ const sr = ScrollReveal({
   delay: 400,
 })
 
-sr.reveal('.home_greeting', {delay:1200, origin:'bottom'})
+/*sr.reveal('.home_greeting', {delay:1200, origin:'bottom'})
 sr.reveal('.home_name', {delay:1400, origin:'bottom'})
-sr.reveal('.home_education', {delay:1600, origin:'bottom'})
+sr.reveal('.home_education', {delay:1600, origin:'bottom'})*/
 sr.reveal('.home_data')
 sr.reveal('.home_handle', {delay: 700})
 sr.reveal('.home_handle, .home_scroll' ,{delay: 700, origin: 'bottom'})
-sr.reveal('.about_container', {delay: 700, origin: 'bottom'})
+/*sr.reveal('.about_container', {delay: 700, origin: 'bottom'})
 sr.reveal('.about_info, .about_description', {delay: 600, origin:'bottom'})
 sr.reveal('..about_description', {delay:500, origin:'bottom'})
 sr.reveal('.section_subtitle, .section_title', {delay: 200, origin:'bottom'})
@@ -149,7 +149,7 @@ sr.reveal('.skills_container ,.skills_content', {delay:600, origin:'bottom'})
 sr.reveal('.services_container, .services_card', {delay:600, origin:'bottom'})
 sr.reveal('.work_filters, .work_container, .work_card', {delay:800, origin:'bottom'})
 sr.reveal('.testimonial_container', {delay:700, origin:'bottom'})
-sr.reveal('.contact_container, .contact_content', {delay:700, origin:'bottom'})
+sr.reveal('.contact_container, .contact_content', {delay:700, origin:'bottom'})*/
 
 
 // form //
